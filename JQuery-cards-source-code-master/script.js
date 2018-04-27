c7c4e0d3217d68a28ac4703d106c00f805bdc68c
DollyG <dollsgoitse@yahoo.com>
2018-04-27 21:42:42 +0000

    initial commit

---
+++ JQuery-cards-source-code-master/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$("#mydream").click(function(){
+		$("#daydream").slideToggle(2000)
+	})
+})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+ 
+
+ /*	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+
+
+}); */
